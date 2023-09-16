ea327a169c5133a34a3f46f642b7c2088138e9f6
leggiand <beschi09@gmail.com>
2023-09-16 21:29:19 +0200

    First Commit

---
+++ p2p/go.mod
@@ -0,0 +1,3 @@
+module github.com/leggiand/WormGole/server
+
+go 1.15
---
+++ p2p/main.go
@@ -0,0 +1,104 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+)
+
+func main() {
+	for {
+
+		print("[0]listen\n[1]connect\n>")
+		var scan string
+		fmt.Scan(&scan)
+		if scan == "0" {
+			listen()
+			break
+		} else if scan == "1" {
+			connect()
+			break
+		}
+
+	}
+	listen()
+
+}
+func connect() {
+	print("host to connect:")
+	var host string
+	fmt.Scan(&host)
+	print("port to connect:")
+	var port string
+	fmt.Scan(&port)
+	ip, err := net.LookupIP(host)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("trying to connect")
+	connection, err2 := net.Dial("tcp", ip[0].String()+":"+port)
+	if err2 != nil {
+		panic(err)
+	}
+	fmt.Println("connected")
+	read(connection)
+	write(connection)
+
+}p[0].String()
+func listen() {
+	print("port to listen:")
+	var port string
+	fmt.Scan(&port)
+	fmt.Println("Starting...")
+
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer listener.Close()
+	defer fmt.Println("Listener off")
+
+	fmt.Println("listening:")
+
+	connection, err := listener.Accept()
+	remoteIp := connection.RemoteAddr().String()
+	fmt.Println("alive")
+	fmt.Println(remoteIp + " is Connected!!")
+	if err != nil {
+		log.Fatal(err)
+	}
+	read(connection)
+	write(connection)
+
+}
+func read(connection net.Conn) {
+	go func(conn net.Conn) {
+		for {
+			buffer := make([]byte, 2048)
+			_, err := io.ReadAtLeast(conn, buffer, 1)
+			if err != nil {
+				fmt.Println("Connection Closed")
+				os.Exit(0)
+			}
+			fmt.Print("\n~" + string(buffer) + "\n>")
+		}
+	}(connection)
+}
+func write(connection net.Conn) {
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+
+		fmt.Print(">")
+		scanner.Scan()
+		message := scanner.Text()
+
+		connection.Write([]byte(message))
+
+	}
+
+}
